ebaba2648292e426252a74f98a91440789620c7a
Neo <echiang73@gmail.com>
2019-08-14 15:07:44 -0700

    Add UI to input fields, some RegEx, still need to work on RegEx for password

--- src/components/sign-up.js
+++ src/components/sign-up.js
@@ -1,168 +1,194 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React from "react";
+import ReactDOM from "react-dom";
 
-class Signup extends Component {
-	constructor() {
-		super()
-		this.state = {
-			// username: '',
-			// password: '',
-			// confirmPassword: '',
-			firstName: '',
-			lastName: '',
-			username: '',
-			email: '',
-			password: '',
-			confirmPassword: '',
+// import "./styles.css";
 
-		}
-		this.handleSubmit = this.handleSubmit.bind(this)
-		this.handleChange = this.handleChange.bind(this)
-	}
-	handleChange(event) {
-		this.setState({
-			[event.target.name]: event.target.value
-		})
-	}
-	handleSubmit(event) {
-		console.log('sign-up handleSubmit, username: ')
-		console.log(this.state.username)
-		event.preventDefault()
+const txtFieldState = {
+  value: "",
+  valid: true,
+  typeMismatch: false,
+  errMsg: "" //this is where our error message gets across
+};
 
-		//request to server to add a new username/password
-		axios.post('/user/', {
-			// username: this.state.username,
-			// password: this.state.password
-			firstName: this.state.firstName,
-			lastName: this.state.lastName,
-			username: (this.state.firstName + this.state.lastName).toLowerCase(),
-			email: this.state.email,
-			password: this.state.password
-		})
-			.then(response => {
-				console.log(response)
-				if (!response.data.errmsg) {
-					console.log('successful signup')
-					this.setState({ //redirect to login page
-						redirectTo: '/login'
-					})
-				} else {
-					console.log('username already taken')
-				}
-			}).catch(error => {
-				console.log('signup error: ')
-				console.log(error)
+const ErrorValidationLabel = ({ txtLbl }) => (
+  <label htmlFor="" style={{ color: "red" }}>
+    {txtLbl}
+  </label>
+);
 
-			})
-	}
+class Signup extends React.Component {
+  state = {
 
+    firstName: {
+      ...txtFieldState,
+      fieldName: "First Name",
+      required: true,
+      requiredTxt: "First Name is required"
+    },
+    lastName: {
+      ...txtFieldState,
+      fieldName: "Last Name",
+      required: false,
+      requiredTxt: "Last Name is required"
+    },
+    email: {
+      ...txtFieldState,
+      fieldName: "Email",
+      required: true,
+      requiredTxt: "Email is required",
+      formatErrorTxt: "Incorrect email format"
+    },
+    password: {
+      ...txtFieldState,
+      fieldName: "Password",
+      required: true,
+      requiredTxt: "Password is required",
+      formatErrorTxt: "Incorrect password format"
+    },
+    allFieldsValid: false
+  };
 
-render() {
-	return (
-		<div className="SignupForm">
-			<h4>Sign up</h4>
-			<form className="form-horizontal">
-			<div className="form-group">
-					<div className="col-1 col-ml-auto">
-						<label className="form-label" htmlFor="firstName">First Name</label>
-					</div>
-					<div className="col-3 col-mr-auto">
-						<input className="form-input"
-							type="text"
-							id="firstName"
-							name="firstName"
-							placeholder="First Name"
-							value={this.state.firstName}
-							onChange={this.handleChange}
-						/>
-					</div>
-				</div>
-				<div className="form-group">
-					<div className="col-1 col-ml-auto">
-						<label className="form-label" htmlFor="lastName">Last Name</label>
-					</div>
-					<div className="col-3 col-mr-auto">
-						<input className="form-input"
-							type="text"
-							id="lastName"
-							name="lastName"
-							placeholder="Last Name"
-							value={this.state.lastName}
-							onChange={this.handleChange}
-						/>
-					</div>
-				</div>
-				<div className="form-group">
-					<div className="col-1 col-ml-auto">
-						<label className="form-label" htmlFor="email">Email</label>
-					</div>
-					<div className="col-3 col-mr-auto">
-						<input className="form-input"
-							type="email"
-							id="email"
-							name="email"
-							placeholder="Email Address"
-							value={this.state.email}
-							onChange={this.handleChange}
-						/>
-					</div>
-				</div>
-				{/* <div className="form-group">
-					<div className="col-1 col-ml-auto">
-						<label className="form-label" htmlFor="username">Username</label>
-					</div>
-					<div className="col-3 col-mr-auto">
-						<input className="form-input"
-							type="text"
-							id="username"
-							name="username"
-							placeholder="Username"
-							value={this.state.username}
-							onChange={this.handleChange}
-						/>
-					</div>
-				</div> */}
-				<div className="form-group">
-					<div className="col-1 col-ml-auto">
-						<label className="form-label" htmlFor="password">Password: </label>
-					</div>
-					<div className="col-3 col-mr-auto">
-						<input className="form-input"
-							placeholder="password"
-							type="password"
-							name="password"
-							value={this.state.password}
-							onChange={this.handleChange}
-						/>
-					</div>
-				</div>
-				<div className="form-group">
-					<div className="col-1 col-ml-auto">
-						<label className="form-label" htmlFor="password">Confirm Password: </label>
-					</div>
-					<div className="col-3 col-mr-auto">
-						<input className="form-input"
-							placeholder="Password"
-							type="password"
-							name="confirmPassword"
-							value={this.state.confirmPassword}
-							onChange={this.handleChange}
-						/>
-					</div>
-				</div>
-				<div className="form-group ">
-					<div className="col-7"></div>
-					<button
-						className="btn btn-primary col-1 col-mr-auto"
-						onClick={this.handleSubmit}
-						type="submit"
-					>Sign up</button>
-				</div>
-			</form>
-		</div>
+  reduceFormValues = formElements => {
+    const arrElements = Array.prototype.slice.call(formElements); //we convert elements/inputs into an array found inside form element
 
-	)
-}
+    //we need to extract specific properties in Constraint Validation API using this code snippet
+    const formValues = arrElements
+      .filter(elem => elem.name.length > 0)
+      .map(x => {
+        const { typeMismatch } = x.validity;
+        const { name, type, value } = x;
+
+        return {
+          name,
+          type,
+          typeMismatch, //we use typeMismatch when format is incorrect(e.g. incorrect email)
+          value,
+          valid: x.checkValidity()
+        };
+      })
+      .reduce((acc, currVal) => {
+        //then we finally use reduce, ready to put it in our state
+        const { value, valid, typeMismatch, type } = currVal;
+        const { fieldName, requiredTxt, formatErrorTxt } = this.state[
+          currVal.name
+        ]; //get the rest of properties inside the state object
+
+        //we'll need to map these properties back to state so we use reducer...
+        acc[currVal.name] = {
+          value,
+          valid,
+          typeMismatch,
+          fieldName,
+          requiredTxt,
+          formatErrorTxt
+        };
+
+        return acc;
+      }, {});
+
+    return formValues;
+  };
+
+  checkAllFieldsValid = formValues => {
+    return !Object.keys(formValues)
+      .map(x => formValues[x])
+      .some(field => !field.valid);
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    const form = event.target;
+
+    //we need to extract specific properties in Constraint Validation API using this code snippet
+    const formValues = this.reduceFormValues(form.elements);
+    const allFieldsValid = this.checkAllFieldsValid(formValues);
+    //note: put ajax calls here to persist the form inputs in the database.
+
+    //END
+
+    this.setState({ ...formValues, allFieldsValid }); //we set the state based on the extracted values from Constraint Validation API
+  };
+
+  render() {
+    const { firstName, lastName, email, password, allFieldsValid } = this.state;
+    const successFormDisplay = allFieldsValid ? "block" : "none";
+    const inputFormDisplay = !allFieldsValid ? "block" : "none";
+
+    const renderFirstNameValidationError = firstName.valid ? (
+      ""
+    ) : (
+        <ErrorValidationLabel txtLbl={firstName.requiredTxt} />
+      );
+    const renderLastNameValidationError = lastName.valid ? (
+      ""
+    ) : (
+        <ErrorValidationLabel txtLbl={lastName.requiredTxt} />
+      );
+    const renderEmailValidationError = email.valid ? (
+      ""
+    ) : (
+        <ErrorValidationLabel txtLbl={email.typeMismatch ? email.formatErrorTxt : email.requiredTxt} />
+      );
+    const renderPasswordValidationError = password.valid ? (
+      ""
+    ) : (
+        <ErrorValidationLabel txtLbl={password.typeMismatch ? password.formatErrorTxt : password.requiredTxt} />
+      );
+
+    // function checkPassword(str) {
+    //   var regexPassword = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    //   return regexPassword.test(str);
+    // }
+
+    return (
+      <div>
+        <div style={{ display: successFormDisplay }}>
+          <h1 style={{ textAlign: "center" }}>Success!</h1>
+          <p style={{ textAlign: "center" }}>
+            You have successfully submitted a form.
+          </p>
+        </div>
+        <div className="form-input" style={{ display: inputFormDisplay, border: "none" }}>
+          <h3 style={{ textAlign: "center" }}>Homeowner Sign Up Form</h3>
+          <form className="form-inside-input" onSubmit={this.onSubmit} noValidate >
+            <input
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              required pattern="[A-Za-z']+"
+            />
+            <br />
+            {renderFirstNameValidationError}
+            <br />
+            <input
+              type="text"
+              name="lastName"
+              placeholder="Last Name"
+              required pattern="[A-Za-z']+"
+            />
+            <br />
+            {renderLastNameValidationError}
+            <br />
+            <input type="email" name="email" placeholder="Email" required />
+            <br />
+            {renderEmailValidationError}
+            <br />
+            <input type="password" name="password" placeholder="Password" required 
+            // pattern="[/^(?=.*\d)(?=.*[!@#$%*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/]+" 
+            />
+            <br />
+            {renderPasswordValidationError}
+            <br />
+
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Signup
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
